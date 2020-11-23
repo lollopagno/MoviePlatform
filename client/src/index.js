96b4dc5152e18ccc3bead53cf2c0a0bf80bfdcb5
lollo_pagno <lorenzo.pagnini@yahoo.it>
2020-11-23 18:07:13 +0100

    Added persistence

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter as Router} from 'react-router-dom';
-import store from "./redux/store";
+import {store, persistence} from "./redux/store";
 import {Provider} from 'react-redux'
-import './index.css';
+import { PersistGate } from 'redux-persist/integration/react';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import './index.css';
+
 ReactDOM.render(
     <Provider store={store}>
-        <Router>
-            <App/>
-        </Router>
+        <PersistGate persistor={persistence}>
+            <Router>
+                <App/>
+            </Router>
+        </PersistGate>
     </Provider>,
     document.getElementById('root')
 );
