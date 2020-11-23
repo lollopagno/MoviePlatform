ac95e2feeb6f4e4c4be8c52dce3a6d499cd2f7e2
lollo_pagno <lorenzo.pagnini@yahoo.it>
2020-11-23 18:06:07 +0100

    Initialized component email

---
+++ client/src/components/authentication/email/dashboardEmail.js
@@ -0,0 +1,81 @@
+import Button from "@material-ui/core/Button";
+import React, {useState} from "react";
+import {makeStyles} from "@material-ui/core/styles";
+import {Grid, Typography} from "@material-ui/core";
+import {request} from '../../../requests/user'
+import {useSelector} from "react-redux";
+import Box from "@material-ui/core/Box";
+import {store} from "../../../redux/store";
+import {changeTokenEmail, signUpSuccess} from "../../../redux/reducer/userReducer";
+
+const RED = '#ce0018'
+const useStyles = makeStyles((theme) => ({
+    button: {
+        margin: theme.spacing(3, 65, 0, 65),
+        width: 175,
+        height: 50
+    },
+    p: {
+        margin: theme.spacing(15, 0, 0, 0)
+    },
+    infoResendEmail: {
+        margin: theme.spacing(0, 5, 0, 5),
+        color: RED
+    }
+}))
+
+function ResendToken() {
+
+    const classes = useStyles();
+    const name = useSelector(state => state.user.name)
+    const username = useSelector(state => state.user.username)
+    const [infoSend, setInfoSend ] = useState('')
+
+    const params = {
+        username: username
+    }
+
+    return (
+        <div className={classes.root}>
+
+            <Grid container spacing={2} justify={"center"}>
+
+                <Grid item xs={6}>
+                    <Typography variant="body1" gutterBottom className={classes.p}>
+                        Hi {name}, thanks for registering.<br/><br/>
+                        An email has been sent to the specified address to verify your account. <strong>You have 12
+                        hours!</strong><br/>
+                        If the code has expired or the email has not arrived, click below!
+                    </Typography>
+                </Grid>
+                <Grid container justify={"center"}>
+                    <Box className={classes.infoResendEmail} p={1}>
+                        {infoSend}
+                    </Box>
+                </Grid>
+                <Button
+                    type="submit"
+                    variant="contained"
+                    color="primary"
+                    value="resend"
+                    onClick={() => {
+                        request.resendTokenEmail(params).then(res => {
+                            setInfoSend(res.data.message)
+                            // Saved new token
+                            store.dispatch(changeTokenEmail(res.data))
+                        }).catch(err => {
+                            setInfoSend(err.response.data.message)
+                        })
+                    }}
+                    className={classes.button}
+                >
+                    Resend email
+                </Button>
+            </Grid>
+
+        </div>
+
+    )
+}
+
+export default ResendToken;
---
+++ client/src/components/authentication/email/validationEmail.js
@@ -0,0 +1,73 @@
+import React, {useEffect, useState} from 'react'
+import {useSelector} from "react-redux";
+import {request} from "../../../requests/user";
+import {Grid} from "@material-ui/core";
+import Box from "@material-ui/core/Box";
+import {makeStyles} from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import history from "../../../history";
+
+const useStyles = makeStyles((theme) => ({
+    box: {
+        borderRadius: 15,
+        fontSize: 20,
+        margin: theme.spacing(10, 5, 2, 7)
+    },
+    button : {
+        margin: theme.spacing(3, 65, 0, 65),
+        width: 150,
+        height: 50
+    }
+}))
+
+function ValidateEmail() {
+
+    const classes = useStyles();
+    const ERROR = '#f6beb4'
+    const SUCCESS = '#c1ffdb'
+
+    const token = useSelector(state => state.user.tokenEmail)
+    const user = useSelector(state => state.user.username)
+
+    const [infoValidation, setInfoValidation] = useState('')
+    const [colorInfo, setColorInfo] = useState('')
+
+    const params = {
+        token: token,
+        username: user
+    }
+
+    useEffect(() => {
+        request.tokenEmail(params).then((res) => {
+            setInfoValidation(res.data.message)
+            setColorInfo(SUCCESS)
+        }).catch(err => {
+            setInfoValidation(err.response.data.message)
+            setColorInfo(ERROR)
+        })
+    }, [])
+
+    const signIn = (
+        <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            value="submit"
+            onClick={() => {history.push('/signIn')}}
+            className={classes.button}
+        >
+            Sign In
+        </Button>
+    )
+
+    return (
+        <Grid container justify={"center"}>
+            <Box className={classes.box} bgcolor={colorInfo} color="success.contrastText" p={5} m={5}>
+                {infoValidation}
+            </Box>
+            {colorInfo === SUCCESS && signIn}
+        </Grid>
+    )
+}
+
+export default ValidateEmail;
