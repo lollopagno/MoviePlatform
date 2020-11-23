7ab8fefe37e73bae4557aad3d2720ec3414e64f8
lollo_pagno <lorenzo.pagnini@yahoo.it>
2020-11-23 11:30:13 +0100

    Added global var API email

--- server/utils/env.js
+++ server/utils/env.js
@@ -4,4 +4,6 @@ module.exports = {
     mongoUri: process.env.MONGODB_URI,
     serverPort: process.env.PORT,
     JWTSecret: process.env.JWT_SECRET,
+    emailKeyPublic: process.env.EMAIL_APIKEY_PUBLIC,
+    emailKeyPrivate: process.env.EMAIL_APIKEY_PRIVATE
 }
