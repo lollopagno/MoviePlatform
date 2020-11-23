cc020bc6b037f0eb504a500209408e7bda4c963c
lollo_pagno <lorenzo.pagnini@yahoo.it>
2020-11-23 18:05:17 +0100

    Added persistence of store

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,3 +1,25 @@
-import { configureStore } from '@reduxjs/toolkit';
+import {combineReducers} from '@reduxjs/toolkit';
+import userReducer from './reducer/userReducer';
+import thunk from "redux-thunk";
+import {createLogger} from "redux-logger";
+import {applyMiddleware, compose, createStore} from "redux";
+import {persistReducer, persistStore} from "redux-persist";
+import storage from 'redux-persist/lib/storage';
 
-export default configureStore({});
+const combineReduces = combineReducers({
+    user: userReducer,
+})
+
+const middlewares = [thunk, createLogger()]
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['user']
+};
+
+export const store = createStore(persistReducer(persistConfig, combineReduces), composeEnhancers(applyMiddleware(...middlewares)))
+export const persistence = persistStore(store)
+
