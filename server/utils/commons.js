e904d1df5d354cf7b8b5bdeec4fd01ae69faf7a3
lollo_pagno <lorenzo.pagnini@yahoo.it>
2020-11-23 11:28:27 +0100

    Initialized common method server

---
+++ server/utils/commons.js
@@ -0,0 +1,57 @@
+const JWT_SECRET = require('../utils/env').JWTSecret
+const jwt = require('jsonwebtoken');
+
+module.exports = {
+    requestJsonFailed: function (res, statusCode, message) {
+        return res.status(statusCode).json({
+            success: false,
+            message: message,
+        })
+    },
+
+    requestJsonSuccess: function (res, statusCode, message, user, token) {
+        return res.status(statusCode).json({
+            success: true,
+            message: message,
+            user: user,
+            token: token
+        })
+    },
+
+    /**
+     * Generate token to authentication
+     * @param user contain data for TOKEN
+     * @returns {undefined|*} json TOKEN
+     */
+    generateToken: function (user) {
+        const tokenData = {
+            _id: user._id.toString(),
+            name: user.name,
+            email: user.email,
+            username: user.username,
+            admin: user.admin
+        };
+        return jwt.sign(tokenData, JWT_SECRET, {
+            algorithm: "HS256",
+            expiresIn: "1h",
+        });
+    },
+
+    getCleanUser: function (user) {
+        if (!user) {
+            return {};
+        }
+
+        const userClean = user.toJSON();
+
+        return {
+            _id: userClean._id,
+            name: userClean.name,
+            email: userClean.email,
+            username: userClean.username,
+            //admin: userClean.admin,
+            //createdAt: userClean.createdAt,
+            //updatedAt: userClean.updatedAt
+        }
+    }
+}
