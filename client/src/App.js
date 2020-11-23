4a22862d4e5fbf44dc3ff6e6e86b662b0ea03dba
lollo_pagno <lorenzo.pagnini@yahoo.it>
2020-11-23 18:08:04 +0100

    improved

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import React, {useEffect} from 'react';
+import Routes from './routes';
 import './App.css';
+import {request} from './requests/user'
+import {meFromTokenFailure, meFromTokenSuccess} from './redux/reducer/userReducer';
+import {store} from './redux/store'
 
 function App() {
+
+  useEffect(() => {
+
+    let token = sessionStorage.getItem('jwtToken');
+    if (!token || token === '') {
+      return {};
+    }
+
+    request.meFromToken(token).then(res => {
+      sessionStorage.setItem('jwtToken', res.data.token);
+      store.dispatch(meFromTokenSuccess(res.data.user));
+    }).catch(err => {
+      sessionStorage.removeItem('jwtToken');
+      store.dispatch(meFromTokenFailure(err.response.data.message))
+    });
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <Routes/>
+      </div>
   );
 }
-
 export default App;
