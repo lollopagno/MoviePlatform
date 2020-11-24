0148a4ac9aff33a82d01170852f3091274658396
lollo_pagno <lorenzo.pagnini@yahoo.it>
2020-11-24 16:53:06 +0100

    Added path films/popular

--- server/route/route.js
+++ server/route/route.js
@@ -2,6 +2,11 @@ const express = require('express');
 const User = require('../controller/user');
 const Token = require('../controller/token');
 const Email = require('../controller/email');
+
+const Films = require('../controller/tmdb/films');
+//const TVPrograms = require('../controller/tmdb/tv');
+//const Actors = require('../controller/tmdb/actors');
+
 const router = express.Router();
 
 // User
@@ -16,5 +21,8 @@ router.get('/email/validation', (req, res) => Email.validationEmail(req,res));
 router.post('/token/email/confirmation', (req, res) => Token.checkTokenEmail(req, res));
 router.post('/token/email/resend', (req, res) => Token.resendTokenEmail(req, res));
 router.post('/token/authentication/check', (req, res) => Token.checkToken(req, res));
+
+// Movie
+router.get('/tmdb/films/popular', () => Films.popular());
 
 module.exports = router;
