c282591c656496596e5218b97752a5f8b8c78f9b
lollo_pagno <lorenzo.pagnini@yahoo.it>
2020-11-25 11:46:27 +0100

    Improved

--- client/src/route/routesWithoutToken.js
+++ client/src/route/routesWithoutToken.js
@@ -4,8 +4,8 @@ import history from '../history';
 import SignIn from '../components/authentication/signIn/signIn'
 import SignUp from '../components/authentication/signUp/signUp'
 import ResendToken from '../components/authentication/email/dashboardEmail'
-import Error from "../components/error";
 import ValidateEmail from "../components/authentication/email/validationEmail";
+import Error from "../components/error";
 
 export default class RoutesWithoutToken extends Component {
     render() {
@@ -13,11 +13,12 @@ export default class RoutesWithoutToken extends Component {
             <Router history={history}>
                 <Switch>
                     <Redirect exact from="/" to="/signIn"/>
+                    <Redirect exact from="/dashboard" to="/signIn"/>
                     <Route path='/signIn' component={SignIn} exact/>
                     <Route path='/signUp' component={SignUp}/>
                     <Route path='/resendToken' component={ResendToken}/>
                     <Route path='/confirmation' component={ValidateEmail}/>
-                    <Route component={SignIn}/>
+                    <Route component={Error}/>
                 </Switch>
             </Router>
         )
