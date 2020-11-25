f36d04d77e84eebdbd01335b01ffb1d1bbdca27f
lollo_pagno <lorenzo.pagnini@yahoo.it>
2020-11-25 21:17:05 +0100

    Added icon account toolbar

--- client/src/components/dashboard/dashboard.js
+++ client/src/components/dashboard/dashboard.js
@@ -4,6 +4,8 @@ import AppBar from "@material-ui/core/AppBar";
 import {useStyles} from "./styles";
 import Grid from "@material-ui/core/Grid";
 import ButtonToolbar from "./buttonToolbar";
+import IconButton from "@material-ui/core/IconButton";
+import {AccountCircle} from "@material-ui/icons";
 
 function Dashboard() {
 
@@ -15,6 +17,9 @@ function Dashboard() {
             <AppBar position="static">
                 <Toolbar>
                     <ButtonToolbar setMovies={setCardsMovies}/>
+                    <IconButton aria-label="delete" className={classes.account}>
+                        <AccountCircle />
+                    </IconButton>
                 </Toolbar>
             </AppBar>
             <Grid
--- client/src/components/dashboard/styles.js
+++ client/src/components/dashboard/styles.js
@@ -4,6 +4,9 @@ import makeStyles from "@material-ui/core/styles/makeStyles";
 const useStyles = makeStyles((theme) => ({
     root: {
         flexGrow: 1,
+    },
+    account: {
+        color: "#fcfcfc"
     }
 }));
 
