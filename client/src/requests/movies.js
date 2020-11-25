6a66ea81dede79120d2b43c580fa38b48ab5de03
lollo_pagno <lorenzo.pagnini@yahoo.it>
2020-11-25 11:54:07 +0100

    Rename file films to movies

--- client/src/requests/movies.js
+++ client/src/requests/movies.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const API = "http://localhost:3000/api"
 
 const popular = () => {
-    return axios.get(API + '/tmdb/films/popular', )
+    return axios.get(API + '/tmdb/movies/popular', )
 }
 
 export const requestFilm = {popular};
---
+++ server/controller/tmdb/movies.js
@@ -0,0 +1,41 @@
+const KEY = require('../../utils/env').apiKeyTmdb
+const https = require('https');
+
+const utils = require('../../utils/commons')
+const codeStatus = require('../../utils/status')
+
+const API_POPULAR = 'https://api.themoviedb.org/3/movie/popular?api_key='
+const IMAGE = 'https://image.tmdb.org/t/p/w500/'
+
+popular = (req, res) => {
+
+    https.get(API_POPULAR + KEY, (result) => {
+
+        let moviesPopular = [];
+
+        if (result.statusCode === 200) {
+            result.setEncoding('utf8');
+
+            result.on('data', function (data) {
+                JSON.parse(data).results.forEach((movie) => {
+
+                    moviesPopular.push({
+                        _id: movie.id,
+                        title: movie.original_title,
+                        date: movie.release_date,
+                        img: IMAGE + movie.poster_path,
+                        language: movie.original_language,
+                    })
+                });
+                utils.requestJsonSuccess(res, codeStatus.OK, 'Movies found.', moviesPopular)
+            });
+        } else {
+            utils.requestJsonFailed(res, codeStatus.badRequest, 'Movies not found!')
+        }
+    })
+}
+
+module.exports = {
+    popular
+}
+
