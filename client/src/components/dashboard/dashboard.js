8787b62a2caef79a13ed9093a66d5c2899614228
lollo_pagno <lorenzo.pagnini@yahoo.it>
2020-11-28 11:06:14 +0100

    Added logOut after press iconButton

--- client/src/components/dashboard/dashboard.js
+++ client/src/components/dashboard/dashboard.js
@@ -6,18 +6,28 @@ import Grid from "@material-ui/core/Grid";
 import ButtonToolbar from "./utility/buttonToolbar";
 import IconButton from "@material-ui/core/IconButton";
 import {AccountCircle} from "@material-ui/icons";
+import {store} from "../../redux/store";
+import {signOut} from "../../redux/reducer/userReducer";
+import {deleteToken} from "../../redux/reducer/tokenReducer";
+import history from '../../history'
 
 function Dashboard() {
 
     const classes = useStyles();
     const [cardsMovies, setCardsMovies] = useState([]);
 
+    const onClickIcon = () => {
+        store.dispatch(signOut())
+        store.dispatch(deleteToken())
+        history.push('/signIn')
+    }
+
     return (
         <Grid container className={classes.root}>
             <AppBar position="static">
                 <Toolbar>
                     <ButtonToolbar setMovies={setCardsMovies}/>
-                    <IconButton aria-label="delete" className={classes.account}>
+                    <IconButton aria-label="delete" className={classes.account} onClick={onClickIcon}>
                         <AccountCircle />
                     </IconButton>
                 </Toolbar>
