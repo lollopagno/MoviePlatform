1270115e69e56a7324fc539facbc8ddfe3b3319d
lollo_pagno <lorenzo.pagnini@yahoo.it>
2020-11-29 11:17:25 +0100

    Improved UI cards

--- client/src/components/dashboard/utility/card.js
+++ client/src/components/dashboard/utility/card.js
@@ -7,17 +7,29 @@ import IconButton from "@material-ui/core/IconButton";
 import FavoriteIcon from "@material-ui/icons/Favorite";
 import React from "react";
 import makeStyles from "@material-ui/core/styles/makeStyles";
+import {Container} from "@material-ui/core";
+import Grid from "@material-ui/core/Grid";
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme) => ({
     media: {
-        height: 150,
         paddingTop: '56.25%', // 16:9
+        height: 240
     },
     card: {
-        maxWidth: 150,
+        height: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+    },
+    cardContent: {
+        flexGrow: 1,
     },
     typography: {
-        height: 70
+        height: 70,
+        marginTop: theme.spacing(2)
+    },
+    cardGrid: {
+        paddingTop: theme.spacing(8),
+        paddingBottom: theme.spacing(8),
     }
 }));
 
@@ -25,25 +37,36 @@ function Cards(props) {
     const classes = useStyles();
 
     return (
-        <Card className={classes.card}>
-            <CardMedia
-                className={classes.media}
-                image={props.movie.img}
-                title={"Img" + props.movie.title}
-            />
-            <CardContent>
-                <Typography variant="body2" color="textSecondary" component="p"
-                            className={classes.typography}>
-                    <strong>Name</strong>: {props.movie.title}<br/><strong>Language</strong>: {props.movie.language}<br/><strong>Release
-                    date</strong>: {props.movie.date}<br/><strong>Vote</strong>: {props.movie.vote}
-                </Typography>
-            </CardContent>
-            <CardActions disableSpacing>
-                <IconButton aria-label="add to favorites">
-                    <FavoriteIcon/>
-                </IconButton>
-            </CardActions>
-        </Card>
+        <Container className={classes.cardGrid} maxWidth="md">
+            <Grid container spacing={4}>
+                {props.result.data.map((movie) => (
+                    <Grid item xs={12} sm={6} md={4} key={movie._id}>
+                        <Card className={classes.card}>
+                            <CardMedia
+                                className={classes.media}
+                                image={movie.img}
+                                title={"Img" + movie.title}
+                            />
+                            <CardContent className={classes.cardContent}>
+                                <Typography gutterBottom variant="h6" component="h2">
+                                    {movie.title}
+                                </Typography>
+                                <Typography variant="body2" align="left" component="p" className={classes.typography}>
+                                    <strong>Language</strong>: {movie.language}<br/>
+                                    <strong>Release date</strong>: {movie.date}<br/>
+                                    <strong>Vote</strong>: {movie.vote}
+                                </Typography>
+                            </CardContent>
+                            <CardActions disableSpacing>
+                                <IconButton aria-label="add to favorites">
+                                    <FavoriteIcon style={{color: 'red'}}/>
+                                </IconButton>
+                            </CardActions>
+                        </Card>
+                    </Grid>
+                ))}
+            </Grid>
+        </Container>
     )
 }
 
