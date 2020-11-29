dcfc043bf6429c822b5e74758e59132c37790cd6
lollo_pagno <lorenzo.pagnini@yahoo.it>
2020-11-29 14:05:01 +0100

    Refactoring code and deleted the component listItem because defined in other place

--- client/src/components/dashboard/utility/buttonToolbar.js
+++ client/src/components/dashboard/utility/buttonToolbar.js
@@ -1,13 +1,9 @@
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemText from "@material-ui/core/ListItemText";
 import Grid from "@material-ui/core/Grid";
-import React, {useEffect, useState} from "react";
+import React, {useEffect} from "react";
 import makeStyles from "@material-ui/core/styles/makeStyles";
-import MenuItem from "@material-ui/core/MenuItem";
-import Menu from "@material-ui/core/Menu";
 import {requestMovies} from "../../../requests/movies";
 import Cards from './card'
+import ListItemComponent from "./listItemToolbar";
 
 const useStyles = makeStyles((theme) => ({
     toolbarLink: {
@@ -24,15 +20,6 @@ const sectionsMenu = [
     {id: 2, title: 'Actors', value: 'actors'}
 ];
 
-/**
- * Sections movies
- */
-const sectionsMovies = [
-    {id: 0, value: 'Movies'},
-    {id: 1, value: 'Popular'},
-    {id: 2, value: 'Top Rated'},
-    {id: 3, value: 'Upcoming'}
-];
 
 function ButtonToolbar(props) {
 
@@ -41,7 +28,7 @@ function ButtonToolbar(props) {
     useEffect(() => {
         requestMovies.popular().then(res => {
             const movies = <Cards result={res.data}/>
-            props.setMovies(movies)
+            props.setCards(movies)
         }).catch()
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
@@ -50,96 +37,10 @@ function ButtonToolbar(props) {
         <div>
             <Grid container className={classes.toolbarLink}>
                 {sectionsMenu.map((section, item) => (
-                    <ListItemComponent item={section} key={item} setMovies={props.setMovies}/>
+                    <ListItemComponent item={section} key={item} setCards={props.setCards}/>
                 ))}
             </Grid>
         </div>
     )
 }
-
-function ListItemComponent(props) {
-
-    // State for Menu item movies
-    const [anchorEl, setAnchorEl] = useState(null);
-    const [selectedIndex, setSelectedIndex] = useState(1);
-
-    const handleMenuItemClick = (event, index) => {
-        setSelectedIndex(index);
-        setAnchorEl(null);
-
-        switch (event.currentTarget.id) {
-            case('1'):
-                console.log("popular")
-                requestMovies.popular().then(res => {
-                    const movies = <Cards result={res.data}/>
-                    props.setMovies(movies)
-                }).catch()
-                break;
-
-            case('2'):
-                console.log("top_rated")
-                requestMovies.topRated().then(res => {
-                    const movies = <Cards result={res.data}/>
-                    props.setMovies(movies)
-                }).catch()
-                break;
-
-            case('3'):
-                console.log("upcoming")
-                requestMovies.upcoming().then(res => {
-                    const movies = <Cards result={res.data}/>
-                    props.setMovies(movies)
-                }).catch()
-                break;
-
-            default:
-                break;
-        }
-    }
-
-    const handleClose = () => {
-        setAnchorEl(null);
-    };
-
-    const handleClickListItem = (event) => {
-        setAnchorEl(event.currentTarget);
-    };
-
-    return (
-        <Grid key={props.item.id}>
-            <List component="nav" aria-label="Device settings" key={props.item.id}>
-                <ListItem
-                    button
-                    aria-haspopup="true"
-                    aria-controls={props.item.id === 0 ? "lock-menu" : null}
-                    aria-label={props.item.title}
-                    onClick={handleClickListItem}
-                >
-                    <ListItemText
-                        primary={props.item.title}/>
-                </ListItem>
-            </List>
-            <Menu
-                id="lock-menu"
-                anchorEl={props.item.id === 0 ? anchorEl : null}
-                keepMounted
-                open={Boolean(props.item.id === 0 ? anchorEl : null)}
-                onClose={handleClose}
-            >
-                {sectionsMovies.map((option, index) => (
-                    <MenuItem
-                        key={option.id}
-                        id={option.id}
-                        disabled={index === 0}
-                        selected={index === selectedIndex}
-                        onClick={(event) => handleMenuItemClick(event, index)}
-                    >
-                        {option.value}
-                    </MenuItem>
-                ))}
-            </Menu>
-        </Grid>
-    )
-}
-
 export default ButtonToolbar;
