85ddaeaed8104c6bb693f15690bad90781d55fc9
lollo_pagno <lorenzo.pagnini@yahoo.it>
2020-11-30 12:43:33 +0100

    Fix bug parse json

--- server/controller/tmdb/actors.js
+++ server/controller/tmdb/actors.js
@@ -4,38 +4,39 @@ const https = require('https');
 const utils = require('../../utils/commons')
 const codeStatus = require('../../utils/status')
 
-const API_POPULAR = 'https://api.themoviedb.org/3/person/popular?api_key='
+const HOST = 'api.themoviedb.org'
+const PATH = '/3/person/popular?api_key='
 const IMAGE = 'https://image.tmdb.org/t/p/w500/'
 
 popular = (req, res) => {
-    getInfo(res, API_POPULAR + KEY)
-}
 
-function getInfo(res, url) {
-    console.log("API actors")
-    https.get(url, (result) => {
+    const options = {
+        host: HOST,
+        path: PATH + KEY
+    };
 
+    https.get(options, (result) => {
+
+        let allData = '';
         let actors = [];
-        console.log("post")
+
         if (result.statusCode === 200) {
             result.setEncoding('utf8');
-
-            result.on('data', function (data) {
-                try {
-                    data = JSON.parse(data)
-                    data = data.results
-                    data.forEach((actor) => {
-                        actors.push({
-                            _id: actor.id,
-                            name: actor.name,
-                            img: IMAGE + actor.profile_path,
-                            popularity: actor.popularity,
-                            department: actor.known_for_department
-                        })
-                    });
-                    console.log(actors)
-                    utils.requestJsonSuccess(res, codeStatus.OK, 'Actors found.', actors)
-                } catch (err) { console.log("[ERR] "+err)}
+            result.on('data', (data) => {
+                allData += data
+            }).on("error", err => {
+                utils.requestJsonFailed(res, codeStatus.badRequest, err.message)
+            }).on('close', () => {
+                JSON.parse(allData).results.forEach((actor) => {
+                    actors.push({
+                        _id: actor.id,
+                        name: actor.name,
+                        img: IMAGE + actor.profile_path,
+                        popularity: actor.popularity,
+                        department: actor.known_for_department
+                    })
+                });
+                utils.requestJsonSuccess(res, codeStatus.OK, 'Actors found.', actors)
             });
         } else {
             utils.requestJsonFailed(res, codeStatus.badRequest, 'Actors not found!')
