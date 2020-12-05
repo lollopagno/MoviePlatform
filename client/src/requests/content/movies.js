a608f5eb41803cc0957a2ad8ee06c7ac8b7ffd4c
lollo_pagno <lorenzo.pagnini@yahoo.it>
2020-12-05 09:58:53 +0100

    Created directory in requests for content

--- client/src/requests/content/movies.js
+++ client/src/requests/content/movies.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const API = "http://localhost:3000/api"
 
-const popular = () => {
-    return axios.get(API + '/tmdb/movies/popular',)
+const popular = (id) => {
+    return axios.get(API + '/tmdb/movies/popular',{params:{ userId: id}})
 }
 
 const topRated = () => {
