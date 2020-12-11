4f564394bd0b12657fce4140c500ace360f4e4da
lollo_pagno <lorenzo.pagnini@yahoo.it>
2020-12-11 11:32:10 +0100

    Completed UI added contents

--- client/src/components/myProfile/bottomBavigation/categotyContent/actors.js
+++ client/src/components/myProfile/bottomBavigation/categotyContent/actors.js
@@ -1,5 +1,204 @@
-import React from'react'
+import React, {useState} from 'react'
+import Grid from "@material-ui/core/Grid";
+import TextField from "@material-ui/core/TextField";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import TitleIcon from "@material-ui/icons/Title";
+import ThumbUpIcon from "@material-ui/icons/ThumbUp";
+import Button from "@material-ui/core/Button";
+import {Alert} from "@material-ui/lab";
+import SaveIcon from "@material-ui/icons/Save";
+import {makeStyles} from "@material-ui/core/styles";
+import MovieIcon from '@material-ui/icons/Movie';
 
-function Actors(){}
+const useStyles = makeStyles((theme) => ({
+    contText: {
+        marginTop: theme.spacing(5)
+    },
+    form: {
+        width: '100%',
+    },
+    input: {
+        display: 'none',
+    },
+    button: {
+        marginTop: theme.spacing(4),
+    },
+    alert: {
+        marginTop: theme.spacing(2)
+    }
+}));
+
+function Actors() {
+
+    const classes = useStyles()
+
+    // State contents
+    const [field, setField] = useState({
+        title: '',
+        popularity: '',
+        department: '',
+        image: null
+    })
+
+    const [error, setError] = useState({
+        title: false,
+        popularity: false,
+        department: false,
+        image: false
+    })
+
+    const [alertImg, setAlertImg] = useState({
+        text: '',
+        isError: false
+    })
+
+    const onChange = (event) => {
+        const {name, value} = event.target
+        setField({...field, [name]: value})
+    }
+
+    const onImageChange = (event) => {
+        if (event.target.files[0]) {
+            const img = event.target.files[0]
+            setField({...field, image: URL.createObjectURL(img)})
+            setAlertImg({...alertImg, text: 'Image loaded correctly!'})
+        } else {
+            setAlertImg({...alertImg, isError: true, text: "Image not loaded!"})
+        }
+    }
+
+    const onSubmit = (event) => {
+        event.preventDefault()
+        setError({
+            ...error,
+            title: field.title === '',
+            department: field.department === '',
+            popularity: (field.popularity === '' || (parseFloat(field.popularity) > 100 || parseFloat(field.popularity) < 0)),
+            image: field.image === null
+        })
+
+        if (field.image === null) {
+            setAlertImg({...alertImg, isError: true, text: 'Image not loaded!'})
+        }
+
+        if (!error.title && !error.department && !error.popularity && field.image !== null) {
+            console.log('[ON SUBMIT]')
+        }
+    }
+
+    return (
+        <Grid container justify={'center'}>
+            <form noValidate className={classes.form} onSubmit={onSubmit}>
+                <Grid item xs={12} className={classes.contText}>
+                    <TextField
+                        error={error.title}
+                        helperText={error.title ? 'Title must not be empty' : ''}
+                        autoComplete="ftitle"
+                        name="title"
+                        variant="standard"
+                        required
+                        fullWidth
+                        id="title"
+                        label="Title"
+                        autoFocus
+                        value={field.title}
+                        InputProps={{
+                            startAdornment: (
+                                <InputAdornment position="start">
+                                    <TitleIcon/>
+                                </InputAdornment>
+                            ),
+                        }}
+                        onChange={onChange}
+                    />
+                </Grid>
+                <Grid item xs={12} className={classes.contText}>
+                    <TextField
+                        error={error.popularity}
+                        helperText={error.popularity ? 'Popularity must not be empty and not greater than 100' : ''}
+                        autoComplete="fpopularity"
+                        type={"number"}
+                        name="popularity"
+                        variant="standard"
+                        required
+                        fullWidth
+                        id="popularity"
+                        label="Popularity"
+                        autoFocus
+                        value={field.popularity}
+                        InputProps={{
+                            startAdornment: (
+                                <InputAdornment position="start">
+                                    <ThumbUpIcon/>
+                                </InputAdornment>
+                            ), inputProps: {min: 0, max: 100, step: 0.1}
+                        }}
+                        onChange={onChange}
+                    />
+                </Grid>
+                <Grid item xs={12} className={classes.contText}>
+                    <TextField
+                        error={error.department}
+                        helperText={error.department ? 'Department must not be empty' : ''}
+                        autoComplete="fdepartment"
+                        name="department"
+                        variant="standard"
+                        required
+                        fullWidth
+                        id="department"
+                        label="Department"
+                        autoFocus
+                        value={field.department}
+                        InputProps={{
+                            startAdornment: (
+                                <InputAdornment position="start">
+                                    <MovieIcon/>
+                                </InputAdornment>
+                            ),
+                        }}
+                        onChange={onChange}
+                    />
+                </Grid>
+                <Grid container justify={'center'} spacing={2}>
+                    <Grid item xs={6}>
+                        <input
+                            accept="image/*"
+                            className={classes.input}
+                            id="contained-button-file"
+                            multiple
+                            type="file"
+                            onChange={onImageChange}
+                        />
+                        <label htmlFor="contained-button-file">
+                            <Button variant="contained" color="primary" component="span" className={classes.button}>
+                                Upload
+                            </Button>
+                        </label>
+                    </Grid>
+                    {alertImg.text &&
+                    <Alert severity={alertImg.isError ? 'error' : 'success'} className={classes.alert}
+                           variant="standard">
+                        {alertImg.text}
+                    </Alert>}
+                </Grid>
+                <Grid container justify={'center'}>
+                    <Grid item xs={6}>
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            color="primary"
+                            className={classes.button}
+                            value={"submit"}
+                            startIcon={<SaveIcon/>}
+                        >
+                            Save
+                        </Button>
+                    </Grid>
+                </Grid>
+            </form>
+        </Grid>
+    )
+}
 
 export default Actors
--- client/src/components/myProfile/bottomBavigation/categotyContent/moviesTv.js
+++ client/src/components/myProfile/bottomBavigation/categotyContent/moviesTv.js
@@ -8,36 +8,101 @@ import ThumbUpIcon from '@material-ui/icons/ThumbUp';
 import LanguageIcon from '@material-ui/icons/Language';
 import {makeStyles} from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
+import SaveIcon from "@material-ui/icons/Save";
+import {Alert} from "@material-ui/lab";
+import {requestNewContents} from "../../../../requests/content/newContents";
+import {useSelector} from "react-redux";
 
 const useStyles = makeStyles((theme) => ({
     contText: {
         marginTop: theme.spacing(5)
     },
+    form: {
+        width: '100%',
+    },
     input: {
         display: 'none',
     },
-
+    button: {
+        marginTop: theme.spacing(4),
+    },
+    alert: {
+        marginTop: theme.spacing(2)
+    }
 }));
 
 function MoviesTvContents() {
 
     const classes = useStyles()
+    const userId = useSelector(state => state.user.id)
 
     // State contents
-    const [title, setTitle] = useState('')
-    const [date, setReleaseDate] = useState('')
-    const [vote, setVote] = useState('')
-    const [language, setLanguage] = useState('')
+    const [field, setField] = useState({
+        title: '',
+        date: '',
+        language: '',
+        vote: '',
+        image: null
+    })
 
-    const onChangeTitle = () => {
+    const [error, setError] = useState({
+        title: false,
+        date: false,
+        language: false,
+        vote: false,
+        image: false
+    })
+
+    const [alertImg, setAlertImg] = useState({
+        text: '',
+        isError: false
+    })
+
+    const onChange = (event) => {
+        const {name, value} = event.target
+        setField({...field, [name]: value})
+    }
+
+    const onImageChange = (event) => {
+        if (event.target.files[0]) {
+            const img = event.target.files[0]
+            setField({...field, image: URL.createObjectURL(img)})
+            setAlertImg({...alertImg, isError: false, text: 'Image loaded correctly!'})
+        } else {
+            setAlertImg({...alertImg, isError: true, text: "Image not loaded!"})
+        }
+    }
+
+    const onSubmit = (event) => {
+        event.preventDefault()
+        setError({
+            ...error,
+            title: field.title === '',
+            date: field.date === '',
+            language: field.language === '',
+            vote: (field.vote === '' || (parseFloat(field.vote) > 10 || parseFloat(field.vote) < 0)),
+            image: field.image === null
+        })
+
+        if (field.image === null) {
+            setAlertImg({...alertImg, isError: true, text: 'Image not loaded!'})
+        }
+        if (!error.title && !error.date && !error.language && !error.vote && field.image !== null) {
+            requestNewContents.added(userId, field).then((res) => {
+                console.log("RES "+res.data)
+            }).catch((err) => {
+                console.log("ERR "+err.response.data.message)
+            })
+        }
     }
 
     return (
-        <form>
-            <Grid container justify={'center'}>
-                <Grid item xs={3} className={classes.contText}>
+        <Grid container justify={'center'}>
+            <form noValidate className={classes.form} onSubmit={onSubmit}>
+                <Grid item xs={12} className={classes.contText}>
                     <TextField
-                        //helperText={errorName ? 'Name must not be empty' : ''}
+                        error={error.title}
+                        helperText={error.title ? 'Title must not be empty' : ''}
                         autoComplete="ftitle"
                         name="title"
                         variant="standard"
@@ -46,7 +111,7 @@ function MoviesTvContents() {
                         id="title"
                         label="Title"
                         autoFocus
-                        value={title}
+                        value={field.title}
                         InputProps={{
                             startAdornment: (
                                 <InputAdornment position="start">
@@ -54,22 +119,23 @@ function MoviesTvContents() {
                                 </InputAdornment>
                             ),
                         }}
-                        onChange={onChangeTitle}
+                        onChange={onChange}
                     />
                 </Grid>
-                <Grid item xs={3} className={classes.contText}>
+                <Grid item xs={12} className={classes.contText}>
                     <TextField
-                        //helperText={errorName ? 'Name must not be empty' : ''}
+                        error={error.date}
+                        helperText={error.date ? 'Release date must not be empty' : ''}
                         type={"date"}
                         autoComplete="freleaseDate"
-                        name="releaseDate"
+                        name="date"
                         variant="standard"
                         required
                         fullWidth
                         id="releaseDate"
                         label="Release Date"
                         autoFocus
-                        value={date}
+                        value={field.date}
                         InputProps={{
                             startAdornment: (
                                 <InputAdornment position="start">
@@ -77,12 +143,13 @@ function MoviesTvContents() {
                                 </InputAdornment>
                             ),
                         }}
-                        //onChange={onChange}
+                        onChange={onChange}
                     />
                 </Grid>
-                <Grid item xs={3} className={classes.contText}>
+                <Grid item xs={12} className={classes.contText}>
                     <TextField
-                        //helperText={errorName ? 'Name must not be empty' : ''}
+                        error={error.language}
+                        helperText={error.language ? 'Language must not be empty' : ''}
                         autoComplete="flanguage"
                         name="language"
                         variant="standard"
@@ -91,7 +158,7 @@ function MoviesTvContents() {
                         id="language"
                         label="Language"
                         autoFocus
-                        value={language}
+                        value={field.language}
                         InputProps={{
                             startAdornment: (
                                 <InputAdornment position="start">
@@ -99,13 +166,15 @@ function MoviesTvContents() {
                                 </InputAdornment>
                             ),
                         }}
-                        //onChange={onChangee}
+                        onChange={onChange}
                     />
                 </Grid>
-                <Grid item xs={3} className={classes.contText}>
+                <Grid item xs={12} className={classes.contText}>
                     <TextField
-                        //helperText={errorName ? 'Name must not be empty' : ''}
+                        error={error.vote}
+                        helperText={error.vote ? 'Vote must not be empty and not greater than 10' : ''}
                         autoComplete="fvote"
+                        type={"number"}
                         name="vote"
                         variant="standard"
                         required
@@ -113,31 +182,56 @@ function MoviesTvContents() {
                         id="vote"
                         label="Vote"
                         autoFocus
-                        value={vote}
+                        value={field.vote}
                         InputProps={{
                             startAdornment: (
                                 <InputAdornment position="start">
                                     <ThumbUpIcon/>
                                 </InputAdornment>
-                            ),
+                            ), inputProps: {min: 0, max: 10, step: 0.1}
                         }}
-                        //onChange={onChange}
+                        onChange={onChange}
                     />
                 </Grid>
-                <input
-                    accept="image/*"
-                    className={classes.input}
-                    id="contained-button-file"
-                    multiple
-                    type="file"
-                />
-                <label htmlFor="contained-button-file">
-                    <Button variant="contained" color="primary" component="span">
-                        Upload
-                    </Button>
-                </label>
-            </Grid>
-        </form>
+                <Grid container justify={'center'} spacing={2}>
+                    <Grid item xs={6}>
+                        <input
+                            accept="image/*"
+                            className={classes.input}
+                            id="contained-button-file"
+                            multiple
+                            type="file"
+                            onChange={onImageChange}
+                        />
+                        <label htmlFor="contained-button-file">
+                            <Button variant="contained" color="primary" component="span" className={classes.button}>
+                                Upload
+                            </Button>
+                        </label>
+                    </Grid>
+                    {alertImg.text &&
+                    <Alert severity={alertImg.isError ? 'error' : 'success'} className={classes.alert}
+                           variant="standard">
+                        {alertImg.text}
+                    </Alert>}
+                </Grid>
+                <Grid container justify={'center'}>
+                    <Grid item xs={6}>
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            color="primary"
+                            className={classes.button}
+                            value={"submit"}
+                            startIcon={<SaveIcon/>}
+                        >
+                            Save
+                        </Button>
+                    </Grid>
+                </Grid>
+            </form>
+        </Grid>
     )
 }
 
