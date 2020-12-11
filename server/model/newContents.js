e1085d2cdbcf07bb4b74f9afde1d1c8884c2db07
lollo_pagno <lorenzo.pagnini@yahoo.it>
2020-12-11 15:50:00 +0100

    Improved model

--- server/model/newContents.js
+++ server/model/newContents.js
@@ -1,13 +1,14 @@
 const mongoose = require('mongoose');
 const newContents = mongoose.Schema(
     {
+        category: {type: String},
         title: {type: String},
-        releaseDate: {type: Date},      /* only movies/tv */
+        date: {type: Date},      /* only movies/tv */
         language: {type: String},       /* only movies/tv */
         department: {type: String},     /* only actors */
         vote: {type: Number},           /* vote for movies/tv, popularity for actors */
-        image: {data: Buffer, contentType: String},
-        _userId: {type: mongoose.Schema.Types.ObjectId, required: true, ref: 'User'},
+        img: {data: Buffer, contentType: String},
+        _userId: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
     }, {
         versionKey: false
     });
