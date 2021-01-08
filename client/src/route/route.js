0271d95da3f08e3f4d908388b56728ccf2a2ff35
lollo_pagno <lorenzo.pagnini@yahoo.it>
2021-01-08 14:45:28 +0100

    Initialized new file route

--- client/src/route/route.js
+++ client/src/route/route.js
@@ -1,22 +1,22 @@
 import React, {Component} from "react";
 import {Router, Redirect, Switch, Route} from "react-router-dom";
 import history from '../history';
+import SignIn from '../components/authentication/signIn/signIn'
+import SignUp from '../components/authentication/signUp/signUp'
+import {ResendToken} from '../components/authentication/email/funtionality'
 import ValidateEmail from '../components/authentication/email/validation'
-import Dashboard from '../components/dashboard/dashboard'
 import Error from "../components/error";
-import SignIn from "../components/authentication/signIn/signIn";
-import SignUp from "../components/authentication/signUp/signUp";
-import {ResendToken} from "../components/authentication/email/funtionality";
+import Dashboard from "../components/dashboard/dashboard";
 import MyProfile from "../components/myProfile/myProfile";
 import Notice from "../components/notice/notice";
 
-export default class RoutesWithToken extends Component {
+export default class RoutesWithoutToken extends Component {
     render() {
         return (
             <Router history={history}>
                 <Switch>
-                    <Redirect exact from="/" to="/signIn"/>
-                    <Route path='/signIn' component={SignIn}/>
+                    <Redirect exact from="/" to="/dashboard"/>
+                    <Route path='/signIn' component={SignIn} exact/>
                     <Route path='/signUp' component={SignUp}/>
                     <Route path='/resendToken' component={ResendToken}/>
                     <Route path='/confirmation' component={ValidateEmail}/>
